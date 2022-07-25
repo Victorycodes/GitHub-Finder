426a803e2c6501c430ffa2e6ad6c0bf256704c36
Victory Ejimofor <victoryejimofor@gmail.com>
2022-07-25 01:13:28 +0100

    Updating my GitHub Finder

--- ui.js
+++ ui.js
@@ -7,22 +7,24 @@ class UI {
     showProfile(user) {
         this.profile.innerHTML = `
             <div class="card card-body">
-                <div class="row">
+                <div class="main">
                     <div class="col nine">
                         <img class="img-fluid" src="${user.avatar_url}">
                         <a href="${user.html_url}" target="blank" class="btn">View Profile</a>
                     </div>
                     <div class="col">
-                        <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-                        <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-                        <span class="badge badge-success">Followers: ${user.followers}</span>
-                        <span class="badge badge-info">Following: ${user.following}</span> 
+                          <div class="flex">
+                                <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+                                <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+                                <span class="badge badge-success">Followers: ${user.followers}</span>
+                                <span class="badge badge-info">Following: ${user.following}</span> 
+                          </div>
                         <br><br>
                         <ul class="list-group">
-                            <li class="list-group-item">Company: ${user.company}</li>
-                            <li class="list-group-item">Website/Blog: ${user.blog}</li>
-                            <li class="list-group-item">Location: ${user.location}</li>
-                            <li class="list-group-item">Member Since: ${user.created_at}</li>
+                            <li class="list-group-item list1">Company: ${user.company}</li>
+                            <li class="list-group-item list2">Website/Blog: ${user.blog}</li>
+                            <li class="list-group-item list3">Location: ${user.location}</li>
+                            <li class="list-group-item list4">Member Since: ${user.created_at}</li>
                         </ul>
                     </div>
                 </div>
@@ -30,6 +32,31 @@ class UI {
             <h3 class="page-heading">Latest Repos</h3>
             <div id="repos"></div>
         `;
+    }
+
+    // Show user repos
+    showRepos(repos) {
+        let output = '';
+
+        repos.forEach(function(repo) {
+            output += `
+            <div class="card card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                       <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                    </div>
+                    <div class="col-md-6">
+                       <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                       <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+                       <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+
+        // Output repos
+        document.getElementById('repos').innerHTML = output;
     }
 
     // Show alert message
@@ -43,7 +70,7 @@ class UI {
         // Add text
         div.appendChild(document.createTextNode(message));
         // Get parent
-        const container = document.querySelector('.searchContainer');
+        const box = document.querySelector('.searchContainer');
         // Get Search box
         const search = document.querySelector('.search');
         // Insert alert
